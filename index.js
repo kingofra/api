c2413692c455c0131da5a569a1ccf7aadcdfd38d
kingofra <53001941+kingofra@users.noreply.github.com>
2022-11-13 20:10:31 +0700

    s

--- index.js
+++ index.js
@@ -441,7 +441,7 @@ app.post('/checkemail', (req,res) => {
 app.post('/forgotpassword', (req,res) => {
     const passgen = genPassword(5)
     const email = req.body.email;
-    let user_result='';
+    let user_result=getUsername(email);
     console.log(passgen);
     console.log(email);
 //     client.query("SELECT * WHERE email = $1", [email], (err, result, fields) =>{
@@ -449,7 +449,7 @@ app.post('/forgotpassword', (req,res) => {
 //     })
     
     client.query("SELECT username from users WHERE email=$1", [email], (err,result,fields) =>{
-         user_result = result.rows[0].username;
+        user_result = result.rows[0].username;
         console.log(user_result);
 //         if (result.rowCount>0) {
 //             console.log('success')
@@ -613,6 +613,22 @@ app.post('/gettotalWater', (req, res) => {
         }
     })
 })
+
+function getUsername(email) {
+    var result = '';
+    client.query("SELECT username from users WHERE email=$1", [email], (err,result,fields) =>{
+       result = result.rows[0].username;
+       console.log(result);
+//         if (result.rowCount>0) {
+//             console.log('success')
+//             res.send({ status: true });
+//         }else{
+//             res.send({ status: false });
+//             console.log(err)
+//         }
+    })
+    return result;
+}
 
 function genPassword(length) {
     var result = [];
