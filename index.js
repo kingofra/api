c5a364be82c75e4f1a25652b4d674bc8949b2f9c
kingofra <53001941+kingofra@users.noreply.github.com>
2022-12-11 11:43:36 +0700

    update

--- index.js
+++ index.js
@@ -542,7 +542,7 @@ app.post('/login', async (req, res) => {
         try {
             const result = await client.query("SELECT * FROM users WHERE username=$1 AND password=$2", [username, password])
 
-            console.log(result.rows.length);
+            console.log(result);
             if (result.rows.length > 0) {
                 console.log("suc");
                 req.body.username = username;
