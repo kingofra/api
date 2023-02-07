1a5cf4b5d688e1c22e3c30e4651394d852badc03
kingofra <53001941+kingofra@users.noreply.github.com>
2023-02-07 11:14:35 +0700

    SMSMSA

--- index.js
+++ index.js
@@ -582,7 +582,7 @@ app.post('/forgotpasswordsms', (req,res) => {
             console.log("ss");
             var data = JSON.stringify({
             "msisdn": phoneno,
-            "sender": "SMSOTP",
+            "sender": "SMSMSA",
             "message": "ทดสอบการส่งข้อความ"
             });
 
